cddda5cb5d92f1f516647d7c61dc38d3c3f3d62b
DSLibner <78918844+DSLibner@users.noreply.github.com>
2022-04-01 16:05:21 +0200

    channel done

--- DCT/DCT.c
+++ DCT/DCT.c
@@ -1,41 +1,38 @@
 #include "contiki.h"
+#include "dctMatrix.h"
 #include "fastdct8.h"
-#include <math.h>
-#include <vector>
-#include <stdio.h>
 
 /* Create DCT matrix */
 PROCESS(DCT, "DCT process");
 AUTOSTART_PROCESSES(&DCT);
 PROCESS_THREAD(DCT, ev, data)
 {
-    static struct etimer timer;
-    
+    //static struct etimer timer;
     PROCESS_BEGIN();
-    double data[8] = {5,2,25,12,3,1,1,2};
-    etimer_set(&timer, CLOCK_SECOND * 10);
+    DCT_Matrix(TheDCTMatrix);
+    INV_DCT_Matrix(TheInvDCTMatrix);
+    float data[8] = {5,2,25,12,3,1,1,2};
+    //etimer_set(&timer, CLOCK_SECOND * 10);
 
-    while(1) 
-    {
+
         for (size_t i = 0; i < sizeof(data)/sizeof(data[0]); i++)
         {
             printf("%d ", (int)data[i]);
         }
         printf("\n");
-        FastDct8_transform(data);
+        DCT_Transform(data);
         for (size_t i = 0; i < sizeof(data)/sizeof(data[0]); i++)
         {
             printf("%d ", (int)data[i]);
         }
         printf("\n");
-        FastDct8_inverseTransform(data);
+        inv_DCT_Transform(data);
         for (size_t i = 0; i < sizeof(data)/sizeof(data[0]); i++)
         {
             printf("%d ", (int)data[i]);
         }
         printf("\n");
-        PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
-        etimer_reset(&timer);
-    }
+        //PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
+        //etimer_reset(&timer);
     PROCESS_END();
 }
--- DCT/dctMatrix.h
+++ DCT/dctMatrix.h
@@ -1,7 +1,10 @@
 #pragma once
 #include <stdio.h>
-#include <math.h>
-
-double ** DCT_Matrix(void);
-void DCT_Transform(double Matrix[8]);
-void inv_DCT_Transform(double Matrix[8]);
+float TheDCTMatrix[8][8];
+float TheInvDCTMatrix[8][8];
+void INV_DCT_Matrix(float matrix[8][8]);
+void DCT_Matrix(float [8][8]);
+void DCT_Transform(float Matrix[8]);
+void inv_DCT_Transform(float Matrix[8]);
+void DCT_Transform_preload(float Matrix[8], float dctmat[8][8]);
+void inv_DCT_Transform_preload(float Matrix[8], float dctmat[8][8]);
